e9dd662209457683f4a4bbc401cea6f27f3d6c84
Levi (dylan) <128552152+dylanabzr@users.noreply.github.com>
2023-05-10 19:50:18 -0300

    Add files via upload

---
+++ primos.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <inttypes.h>
+
+int main(void) {
+    long long int i;
+    long long int num;
+    long long int sqrtnum;
+    clock_t start, end;
+    double execution_time;
+    printf("Digite um número inteiro para descobrir se ele é primo: ");
+    scanf("%llx", &num);
+    start = clock();
+    if (num <0){
+        num = num * -1;
+    }
+    sqrtnum = sqrt(num);
+    sqrtnum = ceil(sqrtnum);
+    for (i = 3; i <= sqrtnum + 3; i += 2)
+    {
+        if (num%2 == 0 && num != 2)  {
+            printf("O número escolhido não é primo.\n");
+            break;
+        }
+        else {
+            if (num%i == 0 && num != i || num == 1){
+                printf("O número escolhido não é primo.\n");
+                break;
+            }
+            else if (i >= sqrtnum){
+                printf("O número escolhido é primo.\n");
+                break;
+            }
+
+        }
+    }
+    end = clock();
+    execution_time = ((double)(end - start))/CLOCKS_PER_SEC;
+    printf("[%lf segundos para executar]\n", execution_time);
+}
