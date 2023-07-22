5c7857a3f82b1cf23eba58ae0f7cc9e43289c7a5
dylan <128552152+dylanabzr@users.noreply.github.com>
2023-07-22 13:06:06 -0300

    Cleaning code

--- primes.c
+++ primes.c
@@ -2,36 +2,32 @@
 #include <math.h>
 #include <time.h>
 #include <limits.h>
-void IsPrime(unsigned long long int num){
+#include <stdbool.h>
+
+bool IsPrime(unsigned long long int num){
     int i;
     unsigned long long int sqrtnum;
-    sqrtnum = sqrt(num);
-    sqrtnum = ceil(sqrtnum);
+    sqrtnum = ceil(sqrt(num));
     for (i = 3; i <= sqrtnum + 3; i += 2){
 	if (i == 3) {
         	if (num%2 == 0 && num != 2)  {
-            		printf("%llu's not prime.\n", num);
-            		break;
+            		return false;
         	}
-    }
-    else {
+    }else {
             if ((num%i == 0 && num != i) || (num == 1)){
-                printf("%llu's not prime.\n", num);
-                break;
+                return false;
             }
             else if (i >= sqrtnum){
-                printf("%llu's prime.\n", num);
-                break;
+                return true;
             }
 	}
     }
 }
 int main() {
-    unsigned long long int num = 2305843009213693951ULL;
     clock_t start, end;
     double execution_time;
     start = clock();
-    IsPrime(num);
+    printf("%s\n",IsPrime(2305843009213693951ULL)?"True":"False");
     end = clock();
     execution_time = ((double)(end - start))/CLOCKS_PER_SEC;
     printf("[%lf seconds to execute]\n", execution_time);
