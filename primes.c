65fcd765f4c400699d5358f23dae6ab2ff7a3ebc
dylanabzr <dylanab@pm.me>
2023-08-07 12:02:31 -0300

    update

--- primes.c
+++ primes.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
 #include <math.h>
-#include <time.h>
-#include <limits.h>
 #include <stdbool.h>
 
 bool isPrime(unsigned long long int num){
@@ -18,12 +15,4 @@ bool isPrime(unsigned long long int num){
 	}
     }
 }
-int main() {
-    clock_t start, end;
-    double execution_time;
-    start = clock();
-    printf("%s\n",isPrime(2305843009213693951ULL)?"True":"False");
-    end = clock();
-    execution_time = ((double)(end - start))/CLOCKS_PER_SEC;
-    printf("[%lf seconds to execute]\n", execution_time);
-}
+
