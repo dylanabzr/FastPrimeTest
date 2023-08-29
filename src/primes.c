7cfd94ab2dd70b4d14bbf69e1a4f715738ff3c21
dylanabzr <dylanab@pm.me>
2023-08-29 20:25:47 -0300

    Fixing identation

--- src/primes.c
+++ src/primes.c
@@ -3,10 +3,11 @@
 short int isPrime(unsigned long long int num){
     unsigned long long int sqrtnum = ceil(sqrt(num));
     for (int i = 3; i <= sqrtnum + 3; i += 2){
-	    if (i == 3) {
+      if (i == 3) {
         if (num%2 == 0 && num != 2)
           return 0;
-      }else {
+      }
+      else {
           if ((num%i == 0 && num != i) || (num == 1))
             return 0;
           else if (i >= sqrtnum)
