b44fcc9c7561aa491824234f98841e85a91dafc4
dylanabzr <dylanab@pm.me>
2023-10-07 01:47:13 -0300

    improved speed

--- src/primes.c
+++ src/primes.c
@@ -1,18 +1,11 @@
 #include <math.h>
 
-short int isPrime(unsigned long long int num){
-    unsigned long long int sqrtnum = ceil(sqrt(num));
-    for (int i = 3; i <= sqrtnum + 3; i += 2){
-      if (i == 3) {
-        if (num%2 == 0 && num != 2)
-          return 0;
-      }
-      else {
-          if ((num%i == 0 && num != i) || (num == 1))
-            return 0;
-          else if (i >= sqrtnum)
-            return 1;
-	}
-    }
+char isPrime(unsigned long long int num){
+  if (!(num%2) && num < 2) return 0;
+  if (num == 2) return 1;
+  unsigned long long int sqrtnum = sqrt(num);
+  for (int i = 3; i <= sqrtnum; i += 2){
+    if (!(num%i)) return 0;
+  }
   return 1;
 }
