0be4c9b43ed208291fbf61c4d1df1e7068a9eafa
dylanabzr <dylanab@pm.me>
2023-10-07 02:09:57 -0300

    FASTER

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main() {
     clock_t start, end;
     double execution_time;
     start = clock();
-    printf("%s\n",isPrime(2305843009213693951ULL)?"True":"False");
+    printf("%s\n",isPrime(2305843009213693951ULL)?"Is prime":"Is not prime");
     end = clock();
     execution_time = ((double)(end - start))/CLOCKS_PER_SEC;
     printf("[%lf seconds to execute]\n", execution_time);
--- src/primes.c
+++ src/primes.c
@@ -4,8 +4,15 @@ char isPrime(unsigned long long int num){
   if (!(num%2) && num < 2) return 0;
   if (num == 2) return 1;
   unsigned long long int sqrtnum = sqrt(num);
-  for (int i = 3; i <= sqrtnum; i += 2){
+  unsigned long long i = 5;
+  int w = 2;
+  int skip_values[23] = {1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+  int skip_index = 0;
+  while(i<= sqrtnum){
     if (!(num%i)) return 0;
+    i+= w;
+    w = skip_values[skip_index];
+    skip_index = (skip_index + 1)%23;
   }
   return 1;
 }
